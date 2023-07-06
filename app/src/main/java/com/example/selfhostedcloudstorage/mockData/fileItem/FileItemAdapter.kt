be6b565adec8a7fcdf81505f1b52cdc701b65764
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2023-07-06 23:53:33 +0200

    fileItems are clickable

--- app/src/main/java/com/example/selfhostedcloudstorage/mockData/fileItem/FileItemAdapter.kt
+++ app/src/main/java/com/example/selfhostedcloudstorage/mockData/fileItem/FileItemAdapter.kt
@@ -4,16 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.selfhostedcloudstorage.R
 
-class FileItemAdapter (private val fileList: List<FileItemViewModel>) :
-    RecyclerView.Adapter<FileItemAdapter.FileViewHolder>() {
+class FileItemAdapter(
+    private val fileList: List<FileItemViewModel>
+) : RecyclerView.Adapter<FileItemAdapter.FileViewHolder>() {
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): FileViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.file_item, parent, false)
         return FileViewHolder(itemView)
@@ -30,6 +29,17 @@ class FileItemAdapter (private val fileList: List<FileItemViewModel>) :
 
     inner class FileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val descriptionTextView: TextView = itemView.findViewById(R.id.descriptionTextView)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val fileItem = fileList[position]
+                    val message = "${fileItem.name} clicked"
+                    Toast.makeText(itemView.context, message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
         fun bind(fileItem: FileItemViewModel) {
             descriptionTextView.text = fileItem.name
